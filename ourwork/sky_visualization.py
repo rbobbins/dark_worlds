77cbef852dac72c49e61f2f3f20d3d43827ae6c8
Rachel <rachelheidi@gmail.com>
2012-11-06 11:22:33 -0500

    Can now visualize a sky with its elliptical galaxies

--- ourwork/sky_visualization.py
+++ ourwork/sky_visualization.py
@@ -1,5 +1,8 @@
 import numpy as np
+# import math
 import matplotlib.pyplot as plt
+from pylab import figure, show, rand
+from matplotlib.patches import Ellipse 
 
 class Galaxy:
   def __init__(self, x, y, e1, e2):
@@ -11,9 +14,11 @@ class Galaxy:
 
   def determine_axes(self):
     theta = np.arctan(self.e2/self.e1) / 2
+    self.theta = theta * 180 / np.pi
+
     E = self.e1 / np.cos(2 * theta)
-    self.a = 1/(1-E)
-    self.b = 1/(1 + E)
+    self.a = 50/(1-E)
+    self.b = 50/(1 + E)
 
 
 class Sky:
@@ -37,7 +42,7 @@ def file_len(fname):
       pass
   return i + 1
 
-def process_data():
+def objectify_data():
   n_skies=file_len('../data/Test_haloCounts.csv')-1 #The number of skies in total
   halos=3 #Number of halos in the most complicated sky
   position_halo=np.zeros([n_skies,2,halos],float) #The array in which
@@ -64,11 +69,19 @@ def process_data():
   return res
 
 def plot_data():
-  data = process_data()
+  data = objectify_data()
 
   for sky in data:
-    for galaxy in sky.galaxies:
-      print galaxy.x, galaxy.y, galaxy.a, galaxy.b
+    fig = figure()
+    ax = fig.add_subplot(111, aspect='equal')
+    for gal in sky.galaxies:
+      e = Ellipse((gal.x, gal.y), gal.a, gal.b, angle=gal.theta, linewidth=2, fill=True)
+      ax.add_artist(e)
+      e.set_clip_box(ax.bbox)
+    ax.set_xlim(0, 4200)
+    ax.set_ylim(0, 4200)
+    show()
+
 
 if __name__ == "__main__":
   plot_data()
