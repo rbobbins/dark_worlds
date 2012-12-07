4626a1a93ba3afbc4e474e174b89338a58119260
Rachel <rachelheidi@gmail.com>
2012-12-06 19:00:43 -0500

    Requires user input about whether to use training or test data, so we don't waste hours procssing the wrong dataset

--- ourwork/everything.py
+++ ourwork/everything.py
@@ -20,7 +20,7 @@ def generate_halo_mag_data(fname, sky_range=None, scaling_factor=1 ):
     print row
 
 
-def objectify_data(test=True, sky_range=None):
+def objectify_data(sky_range=None):
   """ Creates a list of skies with all sky, galaxy, and all known halo 
       information.
       (Only contains halo information if reading from the training dataset)
@@ -31,7 +31,15 @@ def objectify_data(test=True, sky_range=None):
 
       returns: list of skies
   """
-
+  foo = raw_input("Type 'train' for training data, 'test' for test data")
+  if foo == 'test':
+    test = True
+  else if foo = 'train':
+    test = False
+  else:
+    print "Invalid entry"
+    return
+    
   if test:
     n_skies_filename = '../data/Test_haloCounts.csv'
   else:
